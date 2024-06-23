de3cefa00d8fe6ade8b374eaaa69b700769f2f73
EOWNERDEAD <eownerdead@disroot.org>
2024-06-23 18:40:08 +0900

    Init flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        formatter = pkgs.nixfmt-rfc-style;
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            editorconfig-checker
+            nixfmt-rfc-style
+          ];
+        };
+      }
+    );
+}
