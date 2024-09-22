33532ed61d7324a7b59274631cd0f4476e7c5ed5
EOWNERDEAD <eownerdead@disroot.org>
2024-09-22 22:40:51 +0900

    Use writer monad in A.hs

--- src/SF2TAL/A.hs
+++ src/SF2TAL/A.hs
@@ -1,18 +1,21 @@
 module SF2TAL.A (aProg) where
 
 import Control.Monad
-import Data.Foldable
+import Control.Monad.Writer
 import Lens.Micro.Platform
 import SF2TAL.Common
 import SF2TAL.Middle
+
+
+type AT = WriterT [Decl]
 
 
 errorH :: Show a => a -> b
 errorH x = error $ "not in H: " <> show x
 
 
-let' :: [Decl] -> Tm -> Tm
-let' ds e = foldr Let e ds
+let' :: Monad m => AT m Tm -> m Tm
+let' m = uncurry (foldr Let) <$> runWriterT m
 
 
 aTy :: Ty -> Ty
@@ -34,64 +37,56 @@ aHval v = error $ "unannotated: " <> show v
 
 
 aExp :: MonadUniq m => Tm -> m Tm
-aExp (Let d e) = let' <$> aDec d <*> aExp e
-aExp (App v [] vs) = do
-  (ds, v') <- aVal v
-  (ds', vs') <- mapAndUnzipM aVal vs
-  pure $ let' (ds <> fold ds') $ App v' [] vs'
+aExp (Let d e) = let' $ aDec d >> lift (aExp e)
+aExp (App v [] vs) = let' $ App <$> aVal v <*> pure [] <*> traverse aVal vs
 aExp e@App{} = errorH e
-aExp (If0 v e1 e2) = do
-  (ds, v') <- aVal v
-  let' ds <$> (If0 v' <$> aExp e1 <*> aExp e2)
-aExp (Halt t v) = do
-  (ds, v') <- aVal v
-  pure $ let' ds $ Halt (aTy t) v'
+aExp (If0 v e1 e2) = let' $ If0 <$> aVal v <*> lift (aExp e1) <*> lift (aExp e2)
+aExp (Halt t v) = let' $ Halt (aTy t) <$> aVal v
 
 
-aDec :: MonadUniq m => Decl -> m [Decl]
+aDec :: MonadUniq m => Decl -> AT m ()
 aDec (Bind x v) = do
-  (ds, v') <- aVal v
-  pure $ ds <> [Bind x v']
+  v' <- aVal v
+  tell [Bind x v']
 aDec (At x i v) = do
-  (ds, v') <- aVal v
-  pure $ ds <> [At x i v']
+  v' <- aVal v
+  tell [At x i v']
 aDec (Arith x p v1 v2) = do
-  (ds1, v1') <- aVal v1
-  (ds2, v2') <- aVal v2
-  pure $ ds1 <> ds2 <> [Arith x p v1' v2']
+  v1' <- aVal v1
+  v2' <- aVal v2
+  tell [Arith x p v1' v2']
 aDec (Unpack a x v) = do
-  (ds, v') <- aVal v
-  pure $ ds <> [Unpack a x v']
+  v' <- aVal v
+  tell [Unpack a x v']
 aDec d@Malloc{} = errorH d
 aDec d@Update{} = errorH d
 
 
-aVal :: MonadUniq m => Ann -> m ([Decl], Ann)
+aVal :: MonadUniq m => Ann -> AT m Ann
 aVal (u `Ann` t) = case u of
-  Var x -> pure ([], Var x `Ann` aTy t)
-  IntLit i -> pure ([], IntLit i `Ann` aTy t)
+  Var x -> pure $ Var x `Ann` aTy t
+  IntLit i -> pure $ IntLit i `Ann` aTy t
   v `AppT` ts -> do
-    (ds, v') <- aVal v
-    pure (ds, (v' `AppT` aTy ts) `Ann` aTy t)
+    v' <- aVal v
+    pure $ (v' `AppT` aTy ts) `Ann` aTy t
   Pack t' v t'' -> do
-    (ds, v') <- aVal v
-    pure (ds, Pack (aTy t') v' (aTy t'') `Ann` aTy t)
+    v' <- aVal v
+    pure $ Pack (aTy t') v' (aTy t'') `Ann` aTy t
   Tuple vs -> do
     let n = length vs
     let ts = fmap (aTy . (^. ty)) vs
-    (ds, vs') <- unzip <$> traverse aVal vs
+    vs' <- traverse aVal vs
 
-    y0 <- freshName
-    ys <- replicateM n freshName
-    pure
-      ( fold ds
-          <> [Malloc y0 ts]
-          <> [ Update y (Var y' `Ann` tTupleInitedToN (i - 1) ts) i v'
-             | y <- ys
-             | y' <- y0 : ys
-             | v' <- vs'
-             | i <- [1 ..]
-             ]
-      , Var (last (y0 : ys)) `Ann` aTy t
+    y0 <- lift freshName
+    ys <- replicateM n (lift freshName)
+    writer
+      ( Var (last (y0 : ys)) `Ann` aTy t
+      , Malloc y0 ts
+          : [ Update y (Var y' `Ann` tTupleInitedToN (i - 1) ts) i v'
+            | y <- ys
+            | y' <- y0 : ys
+            | v' <- vs'
+            | i <- [1 ..]
+            ]
       )
   Fix{} -> errorH u
